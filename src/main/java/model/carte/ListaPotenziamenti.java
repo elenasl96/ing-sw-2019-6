ac8f84f592deac5506cf1be22246f7aac50d4ff2
Elena <37112748+elenasl96@users.noreply.github.com>
2019-03-25 16:27:58 +0100

    Update nomi potenziamenti

--- src/main/java/model/carte/ListaPotenziamenti.java
+++ src/main/java/model/carte/ListaPotenziamenti.java
@@ -1,14 +1,15 @@
 package model.carte;
 
 public enum ListaPotenziamenti {
-    MIRINOG("Mirino", 'g', 1),
-    MIRINOB("Mirino", 'b', 2),
-    MIRINOR("Mirino", 'r', 3),
-    RAGGIOCINETICOG("Raggio Cinetico", 'g', 4);
+    MIRINO_G("Mirino", 'g', 1),
+    MIRINO_B("Mirino", 'b', 2),
+    MIRINO_R("Mirino", 'r', 3),
+    RAGGIO_CINETICO_G("Raggio Cinetico", 'g', 4);
 
     private String nome;
     private char munizione;
     private int numero;
+
     ListaPotenziamenti(String nome,char munizione, int num){
         this.nome = nome;
         this.munizione = munizione;
