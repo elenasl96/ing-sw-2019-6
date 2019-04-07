b1e706dd169bf684f622edec7622ea6e48c0be9e
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-04-07 21:53:42 +0200

    Movement class completed

--- src/main/java/model/moves/Move.java
+++ src/main/java/model/moves/Move.java
@@ -1,8 +1,9 @@
 package model.moves;
 
+import exception.InvalidMoveException;
 import exception.NotEnoughAmmoException;
 import model.Player;
 
 public interface Move {
-    void execute(Player p) throws NotEnoughAmmoException;
+    void execute(Player p) throws NotEnoughAmmoException, InvalidMoveException;
 }
--- src/main/java/model/moves/Movement.java
+++ src/main/java/model/moves/Movement.java
@@ -2,15 +2,40 @@ package model.moves;
 
 import exception.InvalidMoveException;
 import model.Player;
+import model.field.Field;
 import model.field.Square;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Movement implements Move{
     private Square destination;
+    private static Field field;
+    private ArrayList<Square> reachList = new ArrayList<>();
+    private int steps;
 
-    public void execute(Player p) {
-         p.setCurrentPosition(destination);
+    public void execute(Player p) throws InvalidMoveException {
+        createReachList(p, steps);
+        if(reachList.contains(this.destination)){
+        }else throw new InvalidMoveException();
+    }
+
+    public void createReachList(Player p, int maxSteps) {
+        if (maxSteps != 0) {
+            for (int i = 0; i < maxSteps; i++) {
+                field.getEdges().forEach(edge -> {
+                    if (edge.getSq1().equals(p.getCurrentPosition())) {
+                        this.reachList.add(edge.getSq2());
+                        createReachList(p, maxSteps-1);
+                    } else if (edge.getSq2().equals(p.getCurrentPosition())) {
+                        this.reachList.add(edge.getSq1());
+                        createReachList(p, maxSteps-1);
+                    }
+
+                });
+            }
+        }
+
     }
 
     public Movement(Square destination){
