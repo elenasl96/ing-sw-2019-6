3e83e6c76f924446cafdaa38eb43d4a794702d4c
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-04-08 11:58:01 +0200

    Added javadoc in Movement

--- src/main/java/model/moves/Movement.java
+++ src/main/java/model/moves/Movement.java
@@ -4,41 +4,105 @@ import exception.InvalidMoveException;
 import model.Player;
 import model.field.Field;
 import model.field.Square;
-
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Implements the movement of the player of any steps on the board
+ * @see Move
+ */
 public class Movement implements Move{
+    /**
+     * The square where the player wants to move
+     */
     private Square destination;
-    private static Field field;
+    /**
+     * The field of the current game
+     */
+    private Field field = new Field();
+    /**
+     * The List of achievable Squares by the player
+     */
     private ArrayList<Square> reachList = new ArrayList<>();
-    private int steps;
+    /**
+     * The maximum number of steps the player can do
+     */
+    private int maxSteps;
 
+    /**
+     * Implement the movement
+     * @param p the player who wants to move
+     * @throws InvalidMoveException if the destination is unreachable for the player
+     */
     public void execute(Player p) throws InvalidMoveException {
-        createReachList(p, steps);
+        createReachList(p, maxSteps, p.getCurrentPosition());
         if(reachList.contains(this.destination)){
+            p.setCurrentPosition(destination);
         }else throw new InvalidMoveException();
     }
 
-    public void createReachList(Player p, int maxSteps) {
+    /**
+     * Creates the List of reachable Squares p can do in maxSteps
+     * @param p The maximum number of steps the player can do
+     * @param maxSteps  the player who want to move
+     * @param newCurrentPosition the Square you take in exam every round of the recursion
+     */
+    public void createReachList(Player p, int maxSteps, Square newCurrentPosition) {
         if (maxSteps != 0) {
             for (int i = 0; i < maxSteps; i++) {
                 field.getEdges().forEach(edge -> {
-                    if (edge.getSq1().equals(p.getCurrentPosition())) {
+                    if (edge.getSq1().equals(newCurrentPosition)){
                         this.reachList.add(edge.getSq2());
-                        createReachList(p, maxSteps-1);
-                    } else if (edge.getSq2().equals(p.getCurrentPosition())) {
+                        createReachList(p, maxSteps-1, edge.getSq2());
+                    } else if (edge.getSq2().equals(newCurrentPosition)){
                         this.reachList.add(edge.getSq1());
-                        createReachList(p, maxSteps-1);
+                        createReachList(p, maxSteps-1, edge.getSq1());
                     }
-
                 });
             }
         }
-
     }
 
-    public Movement(Square destination){
+    /**
+     * The constructor
+     * @param field the current game field
+     */
+    public Movement(Field field){
+        this.field = field;
+    }
+
+    /**
+     * Used getters and setters
+     */
+    public Square getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Square destination) {
         this.destination = destination;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public void setField(Field field) {
+        this.field = field;
+    }
+
+    public List<Square> getReachList() {
+        return reachList;
+    }
+
+    public void setReachList(ArrayList<Square> reachList) {
+        this.reachList = reachList;
+    }
+
+    public int getMaxSteps() {
+        return maxSteps;
+    }
+
+    public void setMaxSteps(int maxSteps) {
+        this.maxSteps = maxSteps;
     }
 }
