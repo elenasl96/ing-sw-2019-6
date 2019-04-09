56f7f6453c2c2af0d7f37edd9b6e73eb7d6eba30
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-04-09 16:52:12 +0200

    Update WeaponDeck.java

--- src/main/java/model/decks/WeaponDeck.java
+++ src/main/java/model/decks/WeaponDeck.java
@@ -1,16 +1,14 @@
 package model.decks;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WeaponDeck {
-    private List<Weapon> weapons;
+    private List<Weapon> weapons = new ArrayList<>();
 
-    //public WeaponDeck(){
-        //this.weapons.add(new Weapon("Distruttore", ));
-    //}
 
     public WeaponDeck() {
-        //TODO initialization of weapon deck from static data
+        this.weapons.add(new Weapon("lock rifle", "Basic effect: Deal 2 damage and 1 mark to 1 target you can see. \n with second lock: Deal 1 mark to a different target you can see."));
     }
 
     public List<Weapon> getWeapons() {
