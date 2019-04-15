8eeb54b39175688934031fef65aacda1470ea8f6
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-04-15 17:11:30 +0200

    Update FullGroupException.java
    Co-Authored-By: Elena <elenasl96@users.noreply.github.com>

--- src/main/java/socket/exceptions/FullGroupException.java
+++ src/main/java/socket/exceptions/FullGroupException.java
@@ -1,8 +1,5 @@
 package socket.exceptions;
 
-import exception.FullMarksException;
-import exception.InvalidMoveException;
-
 public class FullGroupException extends Exception {
     public FullGroupException(){
         super("The group is full");
