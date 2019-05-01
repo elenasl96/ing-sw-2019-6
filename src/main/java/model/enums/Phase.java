b302bc6792afea2e382cf3f2fe49d98388e974f9
Elena <37112748+elenasl96@users.noreply.github.com>
2019-05-01 19:39:21 +0200

    add phase from integer

--- src/main/java/model/enums/Phase.java
+++ src/main/java/model/enums/Phase.java
@@ -1,6 +1,8 @@
 package model.enums;
 
-public enum Phase {
+import java.io.Serializable;
+
+public enum Phase implements Serializable {
     WAIT(0),
     FIRST(1),
     SECOND(2),
@@ -11,5 +13,24 @@ public enum Phase {
     Phase(int id)
     {
         this.id=id;
+    }
+
+    public int getId(){
+       return id;
+    }
+
+    public static Phase fromInteger(int x) {
+        switch(x) {
+            case 0:
+                return WAIT;
+            case 1:
+                return FIRST;
+            case 2:
+                return SECOND;
+            case 3:
+                return RELOAD;
+            default:
+                return WAIT;
+        }
     }
 }
