438859813d67d2941c736895baf5e2578fff357b
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-05-07 12:16:35 +0200

    Create GroupTest.java

---
+++ src/test/java/model/room/GroupTest.java
@@ -0,0 +1,27 @@
+package model.room;
+
+import network.exceptions.UserNotInGroupException;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class GroupTest {
+
+    private Group group;
+    private User user1 = new User("1"),
+            user2 = new User("2"),
+            user3 = new User("3");
+
+    @Test
+    void exceptionTest(){
+        group = new Group(8, 1);
+
+        group.join(user1);
+        group.join(user2);
+
+        assertThrows(UserNotInGroupException.class, () -> {
+            group.leave(user3);
+        });
+    }
+}
