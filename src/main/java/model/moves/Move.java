91a890afc53c9b5000ce9d7cd216663dc512ca31
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-05-15 17:56:13 +0200

    Update Move.java

--- src/main/java/model/moves/Move.java
+++ src/main/java/model/moves/Move.java
@@ -3,7 +3,9 @@ import controller.MoveRequestHandler;
 import exception.InvalidMoveException;
 import model.Player;
 
-public interface Move{
+import java.io.Serializable;
+
+public interface Move extends Serializable {
     void execute(Player player, int groupId) throws InvalidMoveException;
     void handle(MoveRequestHandler moveRequestHandler, int groupId) throws InvalidMoveException;
 }
