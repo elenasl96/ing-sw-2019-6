8296ce69889ebd4c9c8cd99eadfd5f8098077149
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-05-16 19:14:59 +0200

    Create ViewTest.java

---
+++ src/test/java/network/socket/ViewTest.java
@@ -0,0 +1,22 @@
+package network.socket;
+
+import model.room.Group;
+import model.room.Message;
+import model.room.User;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class ViewTest {
+
+    @Mock
+    ClientController mockController;
+
+    @Test
+    void ViewClientTestCoverage(){
+        ViewClient view = new ViewClient(mockController);
+        view.onJoin(new User("1"));
+        view.onMessage(new Message(new Group(5,8),new User("1"),"hi"));
+        view.onLeave(new User("1"));
+        view.onStart();
+    }
+}
