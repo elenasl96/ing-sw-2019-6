03f55aab71c30c70e5f4ca74aba5740a874513cb
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-05-17 19:39:53 +0200

    update NextCurrentPlayer

--- src/main/java/model/PlayerList.java
+++ src/main/java/model/PlayerList.java
@@ -12,6 +12,21 @@ public class PlayerList extends ArrayList<Player> {
         return super.get(counter);
     }
 
+    public void nextCurrentPlayer(int groupID){
+        for(int i=0; i<GameContext.get().getGame(groupID).getPlayers().size(); i++){
+            if(GameContext.get().getGame(groupID).getPlayers().get(i).equals(
+                    GameContext.get().getGame(groupID).getCurrentPlayer())) {
+                //If i overflows the number of players, it comes back to 0
+                if(i+1 == GameContext.get().getGame(groupID).getPlayers().size()) {
+                    GameContext.get().getGame(groupID).setCurrentPlayer(GameContext.get().getGame(groupID).getPlayers().get(0));
+                } else {
+                    GameContext.get().getGame(groupID).setCurrentPlayer(GameContext.get().getGame(groupID).getPlayers().get(i+1));
+                }
+                break;
+            }
+        }
+    }
+
     @Override
     public boolean equals(Object o) {
         return super.equals(o);
