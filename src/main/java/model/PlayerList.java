2c859067b3a1eb2f2ef8e95febbe7e86eb1fc19f
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-05-17 20:21:29 +0200

    Update PlayerList.java

--- src/main/java/model/PlayerList.java
+++ src/main/java/model/PlayerList.java
@@ -12,19 +12,20 @@ public class PlayerList extends ArrayList<Player> {
         return super.get(counter);
     }
 
-    public void nextCurrentPlayer(int groupID){
+    public Player nextCurrentPlayer(int groupID){
+        Player player = null;
         for(int i=0; i<GameContext.get().getGame(groupID).getPlayers().size(); i++){
             if(GameContext.get().getGame(groupID).getPlayers().get(i).equals(
                     GameContext.get().getGame(groupID).getCurrentPlayer())) {
                 //If i overflows the number of players, it comes back to 0
                 if(i+1 == GameContext.get().getGame(groupID).getPlayers().size()) {
-                    GameContext.get().getGame(groupID).setCurrentPlayer(GameContext.get().getGame(groupID).getPlayers().get(0));
+                    player = GameContext.get().getGame(groupID).getPlayers().get(0);
                 } else {
-                    GameContext.get().getGame(groupID).setCurrentPlayer(GameContext.get().getGame(groupID).getPlayers().get(i+1));
+                    player = GameContext.get().getGame(groupID).getPlayers().get(i+1);
                 }
                 break;
             }
-        }
+        } return player;
     }
 
     @Override
