acc495060b0846e38247b3eba400237339c3b510
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-05-31 13:16:06 +0200

    Create WeaponTest.java

---
+++ src/test/java/model/decks/WeaponTest.java
@@ -0,0 +1,13 @@
+package model.decks;
+
+import org.junit.jupiter.api.Test;
+
+public class WeaponTest {
+    @Test
+    void test(){
+        WeaponDeck wd = new WeaponDeck();
+        for(int i = 0; i < 20; i++){
+            System.out.println(wd.pickCard());
+        }
+    }
+}
