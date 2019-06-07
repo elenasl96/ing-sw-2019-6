153fb7d2c40b92f960d59e0565b491b4ecbeca1f
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-06-07 13:45:52 +0200

    cognitive complexity abbassata e oinserito javadoc in LaunchClient

--- src/main/java/network/LaunchClient.java
+++ src/main/java/network/LaunchClient.java
@@ -12,12 +12,61 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
 
-//TODO javadoc
+/**
+ * The Launcher ClientSide
+ * It's the only class that needs to be launched when playing main class
+ * Handles the choice of Socket or RMI and CLI or GUI
+ */
 public class LaunchClient {
+    /**
+     * The constant printed when catching errors
+     */
     private static final String ERROR = "An error occurred, retrying...";
-    public static void main(String[] args){
-        RemoteController clientHandler = null;
 
+    /**
+     * The RemoteController of the Client
+     * Can be either SocketClientHandler or RMIClientHandler
+     * @see RemoteController
+     * @see network.socket.SocketClientHandler
+     * @see network.rmi.RMIClientHandler
+     */
+    private static RemoteController clientHandler;
+
+    /**
+     * The Controller client side
+     * @see ClientController
+     */
+    private static ClientController clientController;
+
+    /**
+     * Main method.
+     * Asks first for Socket or RMI, then CLI or GUI
+     * At last, launches Client Controller's run method
+     * Throws RemoteException in running controller
+     * @param args unused
+     * @see ClientController#run()
+     * @see this#socketOrRMI()
+     * @see this#cliOrGui()
+     */
+    public static void main(String[] args){
+        clientHandler = null;
+        socketOrRMI();
+        clientController = null;
+        cliOrGui();
+        try {
+            clientController.run();
+        } catch (RemoteException e) {
+            System.out.println(ERROR);
+        }
+
+        //Via
+    }
+
+    /**
+     * Handles the choice of Socket or RMI by the user
+     * RMI if the user inserts "yes", CLI otherwise
+     */
+    private static void socketOrRMI(){
         System.out.println(">>> Do you want to use RMI? [yes]\n>>> Default Socket");
         Scanner input = new Scanner(System.in);
         String rmi = input.nextLine();
@@ -50,9 +99,14 @@ public class LaunchClient {
                 }
             }
         }
+    }
 
-        ClientController clientController = null;
-
+    /**
+     * Handles the choice of CLI or GUI by the user
+     * GUI if the user inserts "yes", CLI otherwise
+     */
+    private static void cliOrGui(){
+        Scanner input = new Scanner(System.in);
         System.out.println(">>> Do you want to use the GUI? [yes]\n>>> Default CLI");
         String gui = input.nextLine();
         if(gui.equals("yes")){
@@ -73,12 +127,5 @@ public class LaunchClient {
             }
         }
 
-        try {
-            clientController.run();
-        } catch (RemoteException e) {
-            System.out.println(ERROR);
-        }
-
-        //Via
     }
 }
