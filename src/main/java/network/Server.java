e5f3508fa05ffc5d89781c29da4e76285a27ecf3
Elena <37112748+elenasl96@users.noreply.github.com>
2019-06-12 12:19:45 +0200

    comment timer & fix weapon catch

--- src/main/java/network/Server.java
+++ src/main/java/network/Server.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.net.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
--- src/main/java/network/commands/response/AskInput.java
+++ src/main/java/network/commands/response/AskInput.java
@@ -1,25 +1,16 @@
 package network.commands.response;
 
-import model.moves.Effect;
 import network.commands.Response;
 import network.commands.ResponseHandler;
 
 public class AskInput implements Response {
     String inputType;
     private StringBuilder display;
-    Effect effect;
 
     public AskInput(String inputType){
         this.inputType = inputType;
         this.display = new StringBuilder();
         display.append(inputType);
-    }
-
-    public void append(String string){
-        this.display.append(string);
-    }
-
-    public void setEffect(boolean target) {
     }
 
     public String getInputType(){
