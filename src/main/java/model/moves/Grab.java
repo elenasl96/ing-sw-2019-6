f70570c7387099e2465b205991790832044f4f63
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-06-18 11:43:47 +0200

    Update Grab.java

--- src/main/java/model/moves/Grab.java
+++ src/main/java/model/moves/Grab.java
@@ -23,7 +23,6 @@ public class Grab implements Move{
      * @param p     the player grabbing
      * @param groupID   the current game groupID to access the Game from the GameContext
      * @return  new AskInput request
-     * //TODO non so bene come mai ritorni ciò, ma funziona con le ammotiles?? ORA DOVREBBE
      */
     @Override
     public Response execute(Player p, int groupID) {
