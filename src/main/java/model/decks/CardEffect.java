5a4554f153130f7ee203ab0b11dcae1700f2a351
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-06-18 17:33:29 +0200

    Update CardEffect.java

--- src/main/java/model/decks/CardEffect.java
+++ src/main/java/model/decks/CardEffect.java
@@ -1,5 +1,6 @@
 package model.decks;
 
+import jdk.internal.jline.internal.Nullable;
 import model.Ammo;
 import model.enums.EffectType;
 import model.moves.Effect;
@@ -9,11 +10,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * The effect of Weapons and Powerups
+ */
 public class CardEffect {
     private EffectType effectType;
     private List<Effect> effects = new ArrayList<>();
     private ArrayList<Ammo> cost;
-
+    
     CardEffect(EffectType effectType, Stream<Ammo> ammos){
         if(ammos!=null)
             this.setCost(ammos);
@@ -32,6 +36,7 @@ public class CardEffect {
         this.cost = ammos.collect(Collectors.toCollection(ArrayList::new));
     }
 
+    @Nullable
     public List<Ammo> getCost() {
         return cost;
     }
