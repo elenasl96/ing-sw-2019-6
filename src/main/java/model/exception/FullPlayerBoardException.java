8a747603b4b2b2e209dbf0d4563261a689dfeb2e
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-06-18 17:53:16 +0200

    no javadoc to add

--- src/main/java/model/exception/FullPlayerBoardException.java
+++ src/main/java/model/exception/FullPlayerBoardException.java
@@ -1,5 +1,7 @@
 package model.exception;
 
 public class FullPlayerBoardException extends InvalidMoveException{
-    public FullPlayerBoardException(){ super("PlayerBoard is full"); }
+    public FullPlayerBoardException(){
+        super("PlayerBoard is full");
+    }
 }
--- src/main/java/model/exception/NotEnoughAmmoException.java
+++ src/main/java/model/exception/NotEnoughAmmoException.java
@@ -1,7 +1,6 @@
 package model.exception;
 
 public class NotEnoughAmmoException extends InvalidMoveException {
-
     public NotEnoughAmmoException(){
         super("Not enough ammos!");
     }
