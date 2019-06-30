8472e71ed8e458370eac21da74ea27f4d72fdcaa
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-06-30 13:40:47 +0200

    finished network javadoc

--- src/main/java/network/commands/response/UserCreatedResponse.java
+++ src/main/java/network/commands/response/UserCreatedResponse.java
@@ -4,7 +4,10 @@ import model.room.User;
 import network.commands.Response;
 import network.commands.ResponseHandler;
 
-//TODO javadoc
+/**
+ * Sent as an update when the user is crated
+ * @see network.commands.request.CreateUserRequest
+ */
 public class UserCreatedResponse implements Response {
     public final User user;
 
