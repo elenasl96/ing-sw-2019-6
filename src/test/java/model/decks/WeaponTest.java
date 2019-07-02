4df00dff1a2ff15deb2f29d61f3f45b99641825b
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-07-02 17:05:33 +0200

    Update WeaponTest.java

--- src/test/java/model/decks/WeaponTest.java
+++ src/test/java/model/decks/WeaponTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 public class WeaponTest {
     @Test
     void test(){
+        //Testing pickCard
         WeaponDeck wd = new WeaponDeck();
         for(int i = 0; i < 20; i++){
             System.out.println(wd.pickCard());
