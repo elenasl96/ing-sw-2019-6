f83386838ac61e4fa4a397eddd4ec7f14e7131e5
martinaquarantelli <46750012+martinaquarantelli@users.noreply.github.com>
2019-07-02 15:18:27 +0200

    Update PlayerBoardPanel.java

--- src/main/java/view/gui/PlayerBoardPanel.java
+++ src/main/java/view/gui/PlayerBoardPanel.java
@@ -156,12 +156,8 @@ public class PlayerBoardPanel extends JPanel {
         dropPanel.add(voidPanel);
 
         for(i=10;i<(sizeSerie);i++) {
-            try {
-                dropPanel.add(new JLabel((new ImageIcon(ImageIO.read(new File("src/resources/Drops/drop" + serieDamage[i] +".png"))
-                        .getScaledInstance(60, 60, Image.SCALE_SMOOTH)))));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            dropPanel.add(new JLabel((new ImageIcon(new ImageIcon(this.getClass().getResource("drop" + serieDamage[i] +".png")).getImage()
+                    .getScaledInstance(40, 40, Image.SCALE_SMOOTH)))));
         }
 
         for(i=sizeSerie;i<numDrops;i++) {
