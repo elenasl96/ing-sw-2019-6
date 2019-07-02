b696b926b1e4e0c6b449d5097d8d719fc4e334bc
Elena <37112748+elenasl96@users.noreply.github.com>
2019-07-02 18:01:39 +0200

    fix change phase end reload

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -307,6 +307,7 @@ public class GameController{
                     .replace(" ","").toLowerCase());
             player.getUser().receiveUpdate(update);
         weapon.setStatus(WeaponStatus.LOADED);
+        updatePhase(groupID);
     }
 
     //-------------------------------POWERUPS------------------------------------//
